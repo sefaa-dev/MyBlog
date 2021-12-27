ab11bb1fc4d97304413e74c4e85687965566cf48
Sefa Yılmaz <sefayilmaz582@gmail.com>
2021-12-27 06:22:21 +0300

    Add project files.

---
+++ Blog/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Blog.Data;
+using Blog.Data.Repository;
+using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository _repo;
+        public HomeController(IRepository repo)
+        {
+            _repo = repo;
+        }
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Post()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View(new Post());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Post post)
+        {
+            _repo.AddPost(post);
+
+            if (await _repo.SaveChangesAsync())
+                return RedirectToAction("Index");
+            else
+                return View(post);
+           
+          
+        }
+    }
+}
---
+++ Blog/Startup.cs
@@ -0,0 +1,57 @@
+using Blog.Data;
+using Blog.Data.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog
+{
+    public class Startup
+    {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(_config["DefaultConnection"]));
+
+            services.AddTransient<IRepository, Repository>();
+            //services.AddMvc();
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.UseMvcWithDefaultRoute();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
